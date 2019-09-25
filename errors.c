41c644445761b5a44b20b15daa16dc4cde23f747
gomba66 <danielalejo66@hotmail.com>
2019-09-25 20:18:02 +0000

    I fixed the second case

--- errors.c
+++ errors.c
@@ -4,15 +4,18 @@
  * @id: ID of the error
  * Return: Nothing.
  */
-void errors(int id)
+void errors(int id, char *file)
 {
 	switch (id)
 	{
 	case 1:
+		(void)file;
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 		break;
 	case 2:
+		fprintf(stderr, "Error: Can't open file %s\n", file);
+		exit(EXIT_FAILURE);
 		break;
 	case 3:
 		break;
