428e5a1472a931b945263b0e6cf0fb809370b2a5
gomba66 <danielalejo66@hotmail.com>
2019-09-25 20:18:16 +0000

    I added a new case

--- monty.c
+++ monty.c
@@ -9,17 +9,21 @@ int main(int argc, char **argv)
 {
 	FILE *fp;
 
-	printf("%i %s\n", argc, argv[0]);
+	printf("%i %s\n", argc, argv[1]);
 
 	if (argc != 2)
 	{
-		errors(1);
+		errors(1, "asd");
 	}
 
-	fp = fopen(argv[1], "w+");
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		errors(2, argv[1]);
+	}
 	fprintf(fp, "test testing");
 	fclose(fp);
 
-	printf("testing %s\n", argv[0]);
+	printf("testing %s\n", argv[1]);
 	exit(EXIT_SUCCESS);
 }
