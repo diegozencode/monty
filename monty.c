c136c025c39d513bb70b5a80d7c02c583d22afb4
Diego Monroy <difemo91@gmail.com>
2019-09-25 19:38:47 +0000

    add fopen into main

--- monty.c
+++ monty.c
@@ -1,6 +1,4 @@
 #include "monty.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * main - Entry point
  * @argc: Number of parameters.
@@ -9,12 +7,18 @@
  */
 int main(int argc, char **argv)
 {
+	FILE *fp;
+
 	printf("%i %s\n", argc, argv[0]);
 
 	if (argc != 2)
 	{
 		errors(1);
 	}
+
+	fp = fopen(argv[1], "w+");
+	fprintf(fp, "test testing");
+	fclose(fp);
 
 	printf("testing %s\n", argv[0]);
 	exit(EXIT_SUCCESS);
