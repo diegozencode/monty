96525b75b39968876851e9ec30fc7e1450e40b77
gomba66 <danielalejo66@hotmail.com>
2019-09-26 00:29:02 +0000

    [New] Feature for to use the content of the files

---
+++ my_read.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * my_read - Function that process all the document.
+ * @fp: file that will readed.
+ */
+void my_read(FILE *fp)
+{
+	char *line = NULL;
+	size_t len = 0;
+	char *token1 = NULL;
+	char *token2 = NULL;
+
+
+	while (getline(&line, &len, fp) != -1)
+	{
+		token1 = strtok(line, " \n");
+		token2 = strtok(NULL, " \n");
+		printf("------------\n");
+		printf("token1 --> [%s]\n", token1);
+		printf("token2 --> [%s]\n", token2);
+	}
+	free(line);
+}
