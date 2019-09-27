29d5db92619545f1453a305791956bb43397d286
gomba66 <danielalejo66@hotmail.com>
2019-09-27 00:41:02 +0000

    I fixed betty style

--- my_read.c
+++ my_read.c
@@ -10,13 +10,15 @@ void my_read(FILE *fp)
 	size_t len = 0;
 	char *token1 = NULL;
 	stack_t *stack = NULL;
+	int counter = 0;
 
 	while (getline(&line, &len, fp) != -1)
 	{
+		counter++;
 		token1 = strtok(line, " \n");
 		token2 = strtok(NULL, " \n");
 		if (token1 != NULL)
-			option_opcode(token1, &stack);
+			option_opcode(token1, &stack, counter);
 	}
 	free(line);
 }
@@ -24,36 +26,23 @@ void my_read(FILE *fp)
 /**
  * option_opcode - select the correspond opcode funtion
  * @token1: opcode
+ * @stack: Is the head of the list.
+ * @counter: Is the counter of the current line.
  */
-void option_opcode(char *token1, stack_t **stack)
+void option_opcode(char *token1, stack_t **stack, int counter)
 {
 	unsigned int i = 0;
-	/* stack_t *stack = NULL; */
 
 	instruction_t list[] = {
 		{"push", push},
 		{"pall", pall},
-		 /* {"pint", pint},
-		 * {"pop", pop},
-		 * {"swap", swap},
-		 * {"add", add},
-		 * {"nop", nop},
-		 * {"sub", sub},
-		 * {"div", div},
-		 * {"mul", mul},
-		 * {"mod", mod},
-		 * {"pchar", pchar},
-		 * {"pstr", pstr},
-		 * {"rotl", rotl},
-		 * {"rotr", rotr},
-		 */
 		{NULL, NULL}
 	};
 	while (list[i].opcode != NULL)
 	{
 		if (strcmp(token1, list[i].opcode) == 0)
-			/* printf("Is in list tk1[%s] tk2[%s]", token1, token2); */
-			list[i].f(stack, 1);
+
+			list[i].f(stack, counter);
 		i++;
 	}
 }
